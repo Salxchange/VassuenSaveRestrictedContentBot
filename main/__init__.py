a38a95d1fb0e175fdf41afc4f2b9d2bffd5033ef
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-01-03 21:51:17 +0100

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = ("25164506")
+API_ID = int("25164506")
 API_HASH = ("2771620a92dd0d044e51a787f18565c0")
 BOT_TOKEN = ("6470291664:AAEZTb36MmiNYyRGqPliixsce5EH94byepw")
-SESSION = ("SESSION")
+SESSION = ("BABjxmKpuUjsTZywrQaUn9Dbv-K8S3vNsMBkFHtGD3iD5A33Ehyfh2jv_6Ui0yf_UAM6hmiFGpS805QPcYJwU5yCbKOhrPM6lUsAXeacmyqxlhtHRfP_rOGPQWQ_g9tc9RgdRq9L6Y1CJGC9tok0FXYbhbpcrqNX9lPPhaAoUAO-y2X_4kVqdBYvtW7VBuhBbqmf-yxulQRT4O2ZoYEZGKWisp7e4w_3iIHXQrnbe-cjipvUVW3Hk5DFzo1WPw3jrZyxiL00sIQIgycb15CemQI_HuoJCf6IN1OP3pZj89t05Ijh6V0yLH4h2-cs7j2c4dSMRKvRnJ_JJ3LpqBRXoVKkAAAAAS_eymEA")
 FORCESUB = ("Rokubotz")
-AUTH = int("AUTH")
+AUTH = int("5098097249")
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
